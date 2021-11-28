0e3bfb8d9b7ce0e0a707cde40915b24333a14371
William Hamilton <draxy80@gmail.com>
2021-11-29 02:43:43 +1000

    added og image

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,13 @@ export default function App() {
       <Head>
         <title>William Hamilton - Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="William Hamilton Portfolio - Web Development" />
+        <meta property="og:description"
+          content="A web developer looking to make innovative, creative, and responsive web solutions." />
+        <meta property="og:image" content="https://www.draxybearwd.com/og.png" />
+        <meta property="og:image:alt" content="Portfolio" />
+        <meta property="og:type" content="URL" />
+        <meta property="og:url" content="https://www.draxybearwd.com/" />
       </Head>
       <div>
         <Navbar />
